4a41e27f6897d8a56d8f826a8d8573065c04e6c8
Josh Spicer <hello@joshspicer.com>
2018-03-11 23:55:19 -0400

    started set2

---
+++ set2.py
@@ -0,0 +1,21 @@
+import sys
+from Crypto.Cipher import AES
+
+## SET 2
+## cryptopals.com/sets/2/challenges
+
+# Start 9
+
+# Implementation of PKCS#7 padding
+# msg is a bytearray
+def pkcs7Padding(blockLength, msg):
+    remainingBytes = blockLength % len(msg)
+    theBuffer = bytearray()
+    theBuffer[:] = msg
+    theBuffer += bytearray([chr(remainingBytes)] * remainingBytes)
+    return theBuffer
+
+
+
+
+print pkcs7Padding(20, bytearray("YELLOW SUBMARINE")) #"YELLOW SUBMARINE\x04\x04\x04\x04"
